c9f30720239a1303c7b75c2ced6adf6a38652065
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2018-03-18 10:40:04 +0900

    Add copyright to files in build

--- build/l10n/wrap.pl
+++ build/l10n/wrap.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/env perl
 
 # Movable Type (r) Open Source (C) 2005-2013 Six Apart, Ltd.
+# Copyright (C) 2018 Masahiro IUCHI
 # This program is distributed under the terms of the
 # GNU General Public License, version 2.
 #
