e3f53ff4a5259d4039142bd012e6af3b1fc392ec
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2018-03-24 10:02:08 +0900

    Add copyright to files in lib and plugins

--- lib/MT/TBPing.pm
+++ lib/MT/TBPing.pm
@@ -1,4 +1,5 @@
 # Movable Type (r) Open Source (C) 2001-2013 Six Apart, Ltd.
+# Copyright (C) 2016 Masahiro IUCHI
 # This program is distributed under the terms of the
 # GNU General Public License, version 2.
 #
--- plugins/StyleCatcher/lib/StyleCatcher/Library/Local.pm
+++ plugins/StyleCatcher/lib/StyleCatcher/Library/Local.pm
@@ -1,4 +1,5 @@
 # Movable Type (r) Open Source (C) 2005-2013 Six Apart, Ltd.
+# Copyright (C) 2016, 2018 Masahiro IUCHI
 # This program is distributed under the terms of the
 # GNU General Public License, version 2.
 #
