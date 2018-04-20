c5e96af8fc650a21a2305256ea18ce6e71e36599
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2018-04-20 17:25:08 +0900

    Remove lexicon for FacebookCommenter plugin

--- lib/MT/L10N/ja.pm
+++ lib/MT/L10N/ja.pm
@@ -5155,27 +5155,6 @@ use vars qw( @ISA %Lexicon );
 	'A configuration (mt-config.cgi) file already exists, <a href="[_1]">sign in</a> to Movable Type.' => '構成ファイル(mt-config.cgi)はすでに存在します。Movable Typeに<a href="[_1]">サインイン</a>してください。',
 	'To create a new configuration file using the Wizard, remove the current configuration file and then refresh this page' => 'ウィザードで新しく構成ファイルを作るときは、現在の構成ファイルを別の場所に移動してこのページを更新してください。',
 
-## plugins/FacebookCommenters/config.yaml
-	'Provides commenter registration through Facebook Connect.' => 'Facebookコネクトを利用したコメント投稿者の登録機能を提供します。',
-	'Facebook' => 'Facebook',
-
-## plugins/FacebookCommenters/lib/FacebookCommenters/Auth.pm
-	'Set up Facebook Commenters plugin' => 'Facebook Commentersプラグイン設定',
-	'Authentication failure: [_1], reason:[_2]' => '認証に失敗しました: [_1], 理由:[_2]',
-	'Failed to created commenter.' => 'コメンターの作成に失敗しました。',
-	'Failed to create a session.' => 'コメンターセッションの作成に失敗しました。',
-	'Facebook Commenters needs either Crypt::SSLeay or IO::Socket::SSL installed to communicate with Facebook.' => 'Facebook Commenters を利用するには、Crypt::SSLeay または IO::Socket::SSLのいずれかがインストールされている必要があります。',
-	'Please enter your Facebook App key and secret.' => 'FacebookアプリケーションキーとFacebookアプリケーションシークレットを入力してください。',
-	'Could not verify this app with Facebook: [_1]' => 'Facebookでこのアプリケーションを確認できません: [_1]',
-
-## plugins/FacebookCommenters/tmpl/blog_config_template.tmpl
-	'Facebook Application Key' => 'Facebookアプリケーションキー',
-	'The key for the Facebook application associated with your blog.' => 'ブログ関連付用Facebookアプリケーションキー',
-	'Edit Facebook App' => 'Facebookアプリ編集',
-	'Create Facebook App' => 'Facebookアプリ作成',
-	'Facebook Application Secret' => 'Facebookアプリケーションシークレット',
-	'The secret for the Facebook application associated with your blog.' => 'ブログ関連付用Facebookアプリケーションシークレット',
-
 ## plugins/FormattedText/config.yaml
 	'Manage boilerplate.' => '定型文を管理します。',
 
