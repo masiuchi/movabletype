c088d7907491e46d13d05aa8855b2a95877baa9c
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2024-09-21 21:37:02 +0900

    Remove php files

--- php/tests/InitTestDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-system('MT_CONFIG=mysql-test.cfg perl -It/lib -Ilib -Iextlib -MMT::Test=:db -E "say \"Initialized test DB.\""');
-
-set_include_path(realpath(__DIR__). '/../');
-require_once('mt.php');
-MT::get_instance(1, realpath("t/mysql-test.cfg"));
